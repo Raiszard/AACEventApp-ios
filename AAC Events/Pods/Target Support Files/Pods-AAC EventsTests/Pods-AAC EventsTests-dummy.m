3ab85a48b53dee0129c82654f8c7d733605d78a7
Siar Noorzay <siarnoorzay@gmail.com>
2018-01-02 17:04:42 -0800

    pod install changes

---
+++ AAC Events/Pods/Target Support Files/Pods-AAC EventsTests/Pods-AAC EventsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AAC_EventsTests : NSObject
+@end
+@implementation PodsDummy_Pods_AAC_EventsTests
+@end
---
+++ AAC Events/Pods/Target Support Files/Pods-AAC EventsUITests/Pods-AAC EventsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AAC_EventsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AAC_EventsUITests
+@end
