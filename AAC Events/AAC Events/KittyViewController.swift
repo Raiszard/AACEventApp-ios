33a52c3b7870d0a38e3a88fbb77626295871e47e
Siar Noorzay <siarnoorzay@gmail.com>
2018-01-05 22:43:19 -0800

    view header and a temp launch screen

--- AAC Events/AAC Events/KittyViewController.swift
+++ AAC Events/AAC Events/KittyViewController.swift
@@ -23,10 +23,28 @@ import InteractiveSideMenu
  KittyViewController is a controller relevant one of the side menu items. To indicate this it adopts `SideMenuItemContent` protocol.
  */
 class KittyViewController: UIViewController, SideMenuItemContent {
-
+    @IBOutlet weak var headerContainer: UIView!
+    
     /* Show side menu on menu button click
      */
     @IBAction func openMenu(_ sender: UIButton) {
         showSideMenu()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let testImage = UIImage(named: "tempLogo")
+        let header: ViewHeader = .fromNib()
+
+        let hView = header.createHeader(title: "Agenda", subtitle:nil, imageURL: nil, image: nil, isProfile: false)
+
+        hView.translatesAutoresizingMaskIntoConstraints = false
+        headerContainer.addSubview(hView)
+        
+        headerContainer.addConstraint(NSLayoutConstraint(item: hView, attribute: .top, relatedBy: .equal, toItem: headerContainer, attribute: .top, multiplier: 1.0, constant: 0))
+        headerContainer.addConstraint(NSLayoutConstraint(item: hView, attribute: .leading, relatedBy: .equal, toItem: headerContainer, attribute: .leading, multiplier: 1.0, constant: 0))
+        headerContainer.addConstraint(NSLayoutConstraint(item: headerContainer, attribute: .bottom, relatedBy: .equal, toItem: hView, attribute: .bottom, multiplier: 1.0, constant: 0))
+        headerContainer.addConstraint(NSLayoutConstraint(item: headerContainer, attribute: .trailing, relatedBy: .equal, toItem: hView, attribute: .trailing, multiplier: 1.0, constant: 0))
     }
 }
---
+++ AAC Events/AAC Events/ViewHeader.swift
@@ -0,0 +1,71 @@
+//
+//  ViewHeader.swift
+//  AAC Events
+//
+//  Created by Siar Noorzay on 1/5/18.
+//  Copyright © 2018 Afghan American Conference. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class ViewHeader: UIView {
+
+    @IBOutlet weak var headerImage: UIImageView!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitleLabel: UILabel!
+    
+//    var title: String!
+//    var subtile: String?
+//    var imageURL: String?
+//    var image: UIImage?
+    
+    var isProfile: Bool!
+    
+    func createHeader(title: String, subtitle: String?, imageURL: String?, image: UIImage?, isProfile: Bool!) -> UIView {
+        
+        if subtitle != nil && !subtitle!.isEmpty {
+            headerLabel.isHidden = true
+            titleLabel.isHidden = false
+            subTitleLabel.isHidden = false
+            
+            titleLabel.text = title
+            subTitleLabel.text = subtitle!
+            
+            if isProfile {
+                titleLabel.textAlignment = .center
+                subTitleLabel.textAlignment = .center
+            } else {
+                titleLabel.textAlignment = .left
+                subTitleLabel.textAlignment = .left
+            }
+        } else {
+            headerLabel.isHidden = false
+            titleLabel.isHidden = true
+            subTitleLabel.isHidden = true
+            
+            headerLabel.text = title
+
+        }
+        
+        if imageURL != nil && image == nil {
+            let url = URL(string: imageURL!)
+            headerImage.sd_setImage(with: url, completed: nil)
+            
+        } else if image != nil {
+            headerImage.image = image
+        } else {
+            print("INVALID IMAGE SENT TO ViewHeader")
+        }
+        
+        return self
+    }
+
+    
+}
+extension UIView {
+    class func fromNib<T: UIView>() -> T {
+        return Bundle.main.loadNibNamed(String(describing: T.self), owner: nil, options: nil)![0] as! T
+    }
+}
