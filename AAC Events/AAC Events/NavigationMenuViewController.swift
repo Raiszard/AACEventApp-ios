d46a6050e024f919e91c9aff159eaa5b4340de8e
Siar Noorzay <siarnoorzay@gmail.com>
2018-03-04 19:51:01 -0800

    side menu UI and image cleanup

--- AAC Events/AAC Events/NavigationMenuViewController.swift
+++ AAC Events/AAC Events/NavigationMenuViewController.swift
@@ -25,7 +25,9 @@ import InteractiveSideMenu
 class NavigationMenuViewController: MenuViewController {
 
     let kCellReuseIdentifier = "MenuCell"
-    let menuItems = ["About", "Planning Team", "Donate", "FAQ", "Agenda", "Cohorts", "Sponsor", "Norms", "Speakers", "Attendees", "Initiatives", "Conference Evaluation"]
+    
+    let aacMenus = ["About", "Planning Team", "Donate", "FAQ"]
+    let menuItems =  ["Agenda", "Cohorts", "Sponsor", "Norms", "Speakers", "Attendees", "Initiatives", "Conference Evaluation"]
 
     @IBOutlet weak var tableView: UITableView!
 
@@ -33,12 +35,30 @@ class NavigationMenuViewController: MenuViewController {
         return false
     }
 
+    var isAppUnlocked = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
         // Select the initial row
         tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: false, scrollPosition: UITableViewScrollPosition.none)
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 200))
+        view.backgroundColor = .black
+        let imageV = UIImageView(image: UIImage(named: "sideMenuLogo"))
+        imageV.layer.cornerRadius = imageV.frame.size.width/2
+        imageV.contentMode = .scaleAspectFit
+        imageV.clipsToBounds = true
+        view.addSubview(imageV)
+        var position = view.center
+        position.x = view.center.x/2
+        imageV.center = position
+        tableView.tableHeaderView = view
+        
+        
     }
+    
 }
 
 /*
@@ -47,16 +67,40 @@ class NavigationMenuViewController: MenuViewController {
 extension NavigationMenuViewController: UITableViewDelegate, UITableViewDataSource {
 
     func numberOfSections(in tableView: UITableView) -> Int {
+        
+        if isAppUnlocked { //TODO: use the unlock bool from appD
+            return 2
+        }
         return 1
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return menuItems.count
+
+        if isAppUnlocked {
+            if section == 0 {
+                return aacMenus.count
+            } else if section == 1 {
+                return menuItems.count
+            }
+        }
+        return aacMenus.count
     }
+    
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: kCellReuseIdentifier, for: indexPath)
-        cell.textLabel?.text = menuItems[indexPath.row]
+        
+        if isAppUnlocked {
+            if indexPath.section == 0 {
+                cell.textLabel?.text = aacMenus[indexPath.row]
+            }
+            if indexPath.section == 1 {
+                cell.textLabel?.text = menuItems[indexPath.row]
+            }
+        } else {
+            cell.textLabel?.text = aacMenus[indexPath.row]
+        }
+        cell.textLabel?.textColor = .white
 
         return cell
     }
@@ -66,8 +110,15 @@ extension NavigationMenuViewController: UITableViewDelegate, UITableViewDataSour
         guard let menuContainerViewController = self.menuContainerViewController else {
             return
         }
+        var adjustedPath = indexPath
+        //adjust index
+        if isAppUnlocked {
+            if indexPath.section == 1 {
+                adjustedPath.row = indexPath.row + aacMenus.count
+            }
+        }
 
-        menuContainerViewController.selectContentViewController(menuContainerViewController.contentViewControllers[indexPath.row])
+        menuContainerViewController.selectContentViewController(menuContainerViewController.contentViewControllers[adjustedPath.row])
         menuContainerViewController.hideSideMenu()
     }
 }
