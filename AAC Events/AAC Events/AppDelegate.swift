6bb282b69484170a72fc50d75e9d1c492482e7f8
Siar Noorzay <siarnoorzay@gmail.com>
2018-03-06 20:17:32 -0800

    push notifications

--- AAC Events/AAC Events/AppDelegate.swift
+++ AAC Events/AAC Events/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import OneSignal
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -68,6 +69,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return true
         }
         enrolledSessionIDs = sessions
+        
+        let onesignalInitSettings = [kOSSettingsKeyAutoPrompt: false]
+        
+        // Replace 'YOUR_APP_ID' with your OneSignal App ID.
+        OneSignal.initWithLaunchOptions(launchOptions,
+                                        appId: "70531d2d-609a-4093-9b66-b45b03731a4c",
+                                        handleNotificationAction: nil,
+                                        settings: onesignalInitSettings)
+        
+        OneSignal.inFocusDisplayType = OSNotificationDisplayType.notification;
+        
+        // Recommend moving the below line to prompt for push after informing the user about
+        //   how your app will use them.
+        OneSignal.promptForPushNotifications(userResponse: { accepted in
+            print("User accepted notifications: \(accepted)")
+        })
         return true
     }
 
---
+++ AAC Events/Pods/Target Support Files/Pods-AAC Events-OneSignalNotificationServiceExtension/Pods-AAC Events-OneSignalNotificationServiceExtension-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AAC_Events_OneSignalNotificationServiceExtension : NSObject
+@end
+@implementation PodsDummy_Pods_AAC_Events_OneSignalNotificationServiceExtension
+@end
